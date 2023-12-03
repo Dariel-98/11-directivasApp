42dba8f0c4e48d29c781560758eda634b495a796
Dariel <gomez.b.dariel@gmail.com>
2023-12-03 07:16:43 -0600

    Directiva con Input()

--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -4,9 +4,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductosRoutingModule } from './productos-routing.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [AgregarComponent],
-  imports: [CommonModule, ProductosRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ProductosRoutingModule,
+    ReactiveFormsModule,
+    SharedModule,
+  ],
 })
 export class ProductosModule {}
