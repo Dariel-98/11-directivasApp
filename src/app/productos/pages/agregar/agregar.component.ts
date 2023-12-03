046b05e2dfc3840af6ce35d3ec50c762c8b412f3
Dariel <gomez.b.dariel@gmail.com>
2023-12-03 06:48:05 -0600

    Formulario reactivo

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AgregarComponent {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', Validators.required],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  tieneError(campo: string): boolean {
+    return this.miFormulario.get(campo)?.invalid || false;
+  }
 }
